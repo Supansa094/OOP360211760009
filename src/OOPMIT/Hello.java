3467d68321e617b08ff6b884b87871e35ec0eed8
Supansa <Supansn094@gmail.com>
2018-05-08 15:16:53 +0700

    delete data

--- src/OOPMIT/Hello.java
+++ src/OOPMIT/Hello.java
@@ -6,6 +6,6 @@ public class Hello {
         System.out.println("Hello, My name is Supansa");
         System.out.println("Major: information System");
         System.out.println("Faculty: Management Technology");
-        System.out.println("University: RMUTSV");
+
     }
 }
