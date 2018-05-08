4c11c79e202f4c1598d6be8f067b534cf3de5ae9
Supansa <Supansn094@gmail.com>
2018-05-08 15:13:50 +0700

    update data

--- src/OOPMIT/Hello.java
+++ src/OOPMIT/Hello.java
@@ -4,5 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello, My name is Supansa");
+        System.out.println("Major: information System");
+        System.out.println("Faculty: Management Technology");
+        System.out.println("University: RMUTSV");
     }
 }
